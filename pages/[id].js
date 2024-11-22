a8ca595d66189eebe48fdee14a7e7a294a48cab7
minnggy23 <rubyshemaria@gmail.com>
2024-11-22 13:20:52 -0800

    7th

--- pages/[id].js
+++ pages/[id].js
@@ -1,49 +1,47 @@
-import Layout from '../components/layout';
-import { getAllIds, getData } from '../lib/data';
+import Layout from "../components/layout";
+import { getAllIds, getData } from "../lib/data";
 
-export async function getStaticProps( { params } ) {
-  
+export async function getStaticProps({ params }) {
   const data = await getData(params.id);
 
-  console.log({ data })
+  console.log({ data });
   return {
     props: {
       data,
-    }
+    },
   };
 }
-
 
 export async function getStaticPaths() {
   const paths = await getAllIds();
-  console.log({ paths: JSON.stringify(paths)})
+  console.log({ paths: JSON.stringify(paths) });
   return {
     paths,
-    fallback: false
+    fallback: false,
   };
 }
-export default function Entry({data}){
-   // console.log({relatedPeople})
-   console.log({data})
+export default function Entry({ data }) {
+  // console.log({relatedPeople})
+  console.log({ data });
   //  const postTitle = data.post_title
   //  const secondPost = data.post_title
   //  console.log('runing ID route');
-    return(
-        <Layout>
-          <h1>List of Posts names</h1>
-
-          
-        </Layout>
-    );
+  return (
+    <Layout>
+      <article className="card col-6">
+        <div class="card-body">
+          <h5 class="card-title">{itemData.post_title}</h5>
+          <h6 class="card-subtitle mb-2 text-body-secondary">
+            {itemData.user_login}
+          </h6>
+          <p class="card-text">{itemData.post_content}</p>
+        </div>
+      </article>
+    </Layout>
+  );
 }
 
-
-
-       
-  {/* <div class="card-body">
-    <h5 class="card-title">{itemData.post_title}</h5>
-    <h6 class="card-subtitle mb-2 text-body-secondary">{itemData.user_login}</h6>
-    <p class="card-text">{itemData.post_content}</p>
-    
-  </div> */}
- // </div>
+{
+  /* */
+}
+// </div>
