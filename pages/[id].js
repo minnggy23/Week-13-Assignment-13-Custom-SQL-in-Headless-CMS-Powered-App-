06d819d0fc471a5acea95a481fb2698709108c51
minnggy23 <rubyshemaria@gmail.com>
2024-11-22 13:04:43 -0800

    6th

--- pages/[id].js
+++ pages/[id].js
@@ -25,21 +25,25 @@ export async function getStaticPaths() {
 export default function Entry({data}){
    // console.log({relatedPeople})
    console.log({data})
-   const postTitle = data.post_title
-   const secondPost = data.post_title
-   console.log('runing ID route');
+  //  const postTitle = data.post_title
+  //  const secondPost = data.post_title
+  //  console.log('runing ID route');
     return(
         <Layout>
           <h1>List of Posts names</h1>
-          <div class="card" style="width: 18rem;">
-  <div class="card-body">
-    <h5 class="card-title">{itemData.post_title}</h5>
-    <h6 class="card-subtitle mb-2 text-body-secondary">{itemData.user_login}</h6>
-    <p class="card-text">{itemData.post_content}</p>
-    
-  </div>
-</div>
+
           
         </Layout>
     );
 }
+
+
+
+       
+  {/* <div class="card-body">
+    <h5 class="card-title">{itemData.post_title}</h5>
+    <h6 class="card-subtitle mb-2 text-body-secondary">{itemData.user_login}</h6>
+    <p class="card-text">{itemData.post_content}</p>
+    
+  </div> */}
+ // </div>
